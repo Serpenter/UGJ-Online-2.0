10fc46149d72fb1df7d73aab458b17f497d40f7d
Kostiantyn <knifantiev@gmail.com>
2021-10-02 11:28:43 +0300

    Fixed Y-sort for composed scene

--- Little Red Riding Hood/test_scenes/Blocks.gd
+++ Little Red Riding Hood/test_scenes/Blocks.gd
@@ -10,15 +10,19 @@ export(int) var height = 384
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    var game_objects = get_parent().get_node("GameObjects")
     if Engine.editor_hint:
         for child in get_children():
             child.get_node("Scene").visible = true
     else:
         for child in get_children():
+#            for grandson in child.get_node("Scene/GameObjects").get_children():
+#                game_objects.add_child(grandson)
             if child.get_node("ActiveArea").overlaps_area(get_parent().get_node("GameObjects/Char/ActiveRange")):
                 child.get_node("Scene").visible = true
             else:
                 child.get_node("Scene").visible = false
+
 
 
 func process_children(newVal):
